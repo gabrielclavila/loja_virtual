0df2b0bb942b9ff3bb041a2cd85619dfb57fd480
reitoria <reitoria@10.100.0.224>
2025-01-14 10:24:41 -0400

    Atualização referente a criação da classe de modelo Produto e alteração na classe principal gerada pelo SpringBoot para inserir notação que pode corrigir problemas de não criação de tabelas e atributos no BD.

--- src/main/java/jdev/gab/lojavirtual/LojaVirtualApplication.java
+++ src/main/java/jdev/gab/lojavirtual/LojaVirtualApplication.java
@@ -2,8 +2,10 @@ package jdev.gab.lojavirtual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "jdev.gab.lojavirtual.model")
 public class LojaVirtualApplication {
 
 	public static void main(String[] args) {
