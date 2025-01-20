3ddcb1fad2e10e994d0471972dbfa3bd6f4e61f0
reitoria <reitoria@R19001756>
2025-01-20 19:43:49 -0400

    Atualização referente a alteração no arquivo Application do projeto para inserir novas notações e alteração no arquivo application.properties para inserir novas configurações

--- src/main/java/jdev/gab/lojavirtual/LojaVirtualApplication.java
+++ src/main/java/jdev/gab/lojavirtual/LojaVirtualApplication.java
@@ -3,9 +3,15 @@ package jdev.gab.lojavirtual;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EntityScan(basePackages = "jdev.gab.lojavirtual.model")
+@ComponentScan(basePackages = {"jdev.*"})
+@EnableJpaRepositories(basePackages = {"jdev.gab.lojavirtual.repository"})
+@EnableTransactionManagement
 public class LojaVirtualApplication {
 
 	public static void main(String[] args) {
