26bf7c28a52658e8b0f410f15777c8d96ff6fceb
reitoria <reitoria@R19001756>
2025-01-20 21:13:42 -0400

    Atualização referente alteração na classe de Teste para inserção no Banco via Injeção de Dependência direta pelo Repository

--- src/test/java/jdev/gab/lojavirtual/LojaVirtualApplicationTests.java
+++ src/test/java/jdev/gab/lojavirtual/LojaVirtualApplicationTests.java
@@ -1,13 +1,29 @@
 package jdev.gab.lojavirtual;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import jdev.gab.lojavirtual.model.Acesso;
+import jdev.gab.lojavirtual.repository.AcessoRepository;
+import jdev.gab.lojavirtual.service.AcessoService;
+
+@SpringBootTest(classes = LojaVirtualApplication.class)
 class LojaVirtualApplicationTests {
 
+	@Autowired
+	private AcessoService acessoService;
+	
+	@Autowired
+	private AcessoRepository acessoRepository;
+	
 	@Test
-	void contextLoads() {
+	public void testCadastrarAcesso() {
+		
+		Acesso acesso = new Acesso();
+		
+		acesso.setDescricao("ROLE_ADMIN");
+		acessoRepository.save(acesso);
 	}
 
 }
